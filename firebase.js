7b95c36c50b0c89639d689bf3f8e7530848bb4f3
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-03-08 17:49:30 -0800

    Setup firebase.

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGZ3oSQ3YgTwNfkJ62Q-5GaRZw9ReR1yY",
+  authDomain: "restaurante-9eaa9.firebaseapp.com",
+  projectId: "restaurante-9eaa9",
+  storageBucket: "restaurante-9eaa9.appspot.com",
+  messagingSenderId: "660416228299",
+  appId: "1:660416228299:web:bfb5ed20bf813c6b86a44e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
