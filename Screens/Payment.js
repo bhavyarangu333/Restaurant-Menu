be03392bb0faa3fe1d2fc8c8c561078fc1f002c7
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-04-17 23:27:49 -0700

    More refactoring.

--- Screens/Payment.js
+++ Screens/Payment.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput, SafeAreaView, StyleSheet, Button} from 'react-native';
-import { StripeProvider, CardField, useConfirmPayment, AddressSheet } from '@stripe/stripe-react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, SafeAreaView, StyleSheet, Pressable} from 'react-native';
+import { StripeProvider, CardField, useConfirmPayment, useStripe } from '@stripe/stripe-react-native';
 
 
 const API_URL = 'http://localhost:3000';
@@ -8,66 +8,118 @@ const API_URL = 'http://localhost:3000';
 const Payment = () => {
     const [email, setEmail] = useState();
     const [cardDetails, setCardDetails] = useState();
-    const { confirmPayment, loading } = useConfirmPayment();
+    const { confirmPayment, loadingPayment } = useConfirmPayment();
 
-    
+    const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const [loading, setLoading] = useState(false);
 
-    const fetchPaymentIntentClientSecret = async () => {
-        const response = await fetch(`${API_URL}/create-payment-intent`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+    const fetchPaymentSheetParams = async () => {
+        const response = await fetch(`${API_URL}/payment-sheet`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+        },
         });
-        const { clientSecret, error } = await response.json();
-        return { clientSecret, error };
-      };
+        const { setupIntent, ephemeralKey, customer } = await response.json();
+
+        return {
+        setupIntent,
+        ephemeralKey,
+        customer,
+        };
+    };
+
+    const initializePaymentSheet = async () => {
+        const {
+        setupIntent,
+        ephemeralKey,
+        customer,
+        } = await fetchPaymentSheetParams();
+
+        const { error } = await initPaymentSheet({
+            merchantDisplayName: "Example, Inc.",
+            customerId: customer,
+            customerEphemeralKeySecret: ephemeralKey,
+            setupIntentClientSecret: setupIntent,
+        });
+        if (!error) {
+            setLoading(true);
+        }
+    };
+
+    const openPaymentSheet = async () => {
+        // see below
+        const { error } = await presentPaymentSheet();
+
+        if (error) {
+            alert(`Error code: ${error.code}`, error.message);
+        } else {
+            alert('Payment Saved Successfully', 'Your payment method is successfully set up for future payments!');
+        }
+    };
+
+    useEffect(() => {
+        initializePaymentSheet();
+    }, []);
+
     
 
-    const handleSave = async () => {
-        if (!cardDetails?.complete || !email) {
-            alert("Please enter Complete card details and Email");
-            return;
-          }
-          const billingDetails = {
-            email: email,
-          };
-          //2.Fetch the intent client secret from the backend
-          try {
-            const { clientSecret, error } = await fetchPaymentIntentClientSecret();
-            //2. confirm the payment
-            if (error) {
-              console.log("Unable to process payment");
-            } else {
-              const { paymentIntent, error } = await confirmPayment(clientSecret, {
-                paymentMethodType: "Card",
-                billingDetails: billingDetails,
-              });
-              if (error) {
-                alert(`Payment Confirmation Error ${error.message}`);
-              } else if (paymentIntent) {
-                alert("Payment Successful");
-                console.log("Payment successful ", paymentIntent);
-              }
-            }
-          } catch (e) {
-            console.log(e);
-          }
-    };
+    // const fetchPaymentIntentClientSecret = async () => {
+    //     const response = await fetch(`${API_URL}/create-payment-intent`, {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     });
+    //     const { clientSecret, error } = await response.json();
+    //     return { clientSecret, error };
+    //   };
+    
+
+    // const handleSave = async () => {
+    //     if (!cardDetails?.complete || !email) {
+    //         alert("Please enter Complete card details and Email");
+    //         return;
+    //       }
+    //       const billingDetails = {
+    //         email: email,
+    //       };
+    //       //2.Fetch the intent client secret from the backend
+    //       try {
+    //         const { clientSecret, error } = await fetchPaymentIntentClientSecret();
+    //         //2. confirm the payment
+    //         if (error) {
+    //           console.log("Unable to process payment");
+    //         } else {
+    //           const { paymentIntent, error } = await confirmPayment(clientSecret, {
+    //             paymentMethodType: "Card",
+    //             billingDetails: billingDetails,
+    //           });
+    //           if (error) {
+    //             alert(`Payment Confirmation Error ${error.message}`);
+    //           } else if (paymentIntent) {
+    //             alert("Payment Successful");
+    //             console.log("Payment successful ", paymentIntent);
+    //           }
+    //         }
+    //       } catch (e) {
+    //         console.log(e);
+    //       }
+    // };
 
 
     return (
         <StripeProvider publishableKey='pk_test_51MwZH3AmdXqnDkBiZ9qNGFWUQSY7TttOb7f6ro3nl0sX64NX1G1OKWkZsXxn9yHCss32ENmKOFVMasc7VKLMPyEn00hZpFZdrA'>
             <SafeAreaView style={styles.container}>
 
-            <TextInput
+            {/* <TextInput
                 autoCapitalize="none"
                 placeholder="E-mail"
                 keyboardType="email-address"
                 onChange={value => setEmail(value.nativeEvent.text)}
                 style={styles.input}
-            />
-            <CardField
+            /> */}
+            {/* <CardField
                 placeholders={{
                     number: "4242 4242 4242 4242"
                 }}
@@ -77,9 +129,14 @@ const Payment = () => {
                 onCardChange={cardDetails => {
                     setCardDetails(cardDetails);
                 }}
-            />
+            /> */}
 
-            <Button onPress={handleSave} title='Save' disabled={loading}/>
+            {/* <Button onPress={handleSave} title='Save' disabled={loadingPayment}/> */}
+            
+            <Pressable disabled={!loading} onPress={() => openPaymentSheet()} style={{justifyContent: 'center', alignItems:'center', margin: 10, backgroundColor:'black', height:40, borderRadius:8}}>
+                <Text style={{color:'white'}}>Set Up Payment</Text>
+            </Pressable>
+
             
             </SafeAreaView>
         </StripeProvider>
